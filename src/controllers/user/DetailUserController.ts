b0733ec99da60a5e4102b162f6c816c1ea44d3e5
zeca.rt22 <zeca.rt22@gmail.com>
2022-09-16 21:11:16 -0300

    fix/user details router

--- src/controllers/user/DetailUserController.ts
+++ src/controllers/user/DetailUserController.ts
@@ -3,11 +3,11 @@ import { DetailUserService } from "../../services/user/DetailUserService";
 
 class DetailUserController {
   async handle(req: Request, res: Response) {
-    const user_id = req.user_id;
+    const user_id = req.query.user_id as string;
 
     const detailUserService = new DetailUserService();
 
-    const user = await detailUserService.execute(user_id);
+    const user = await detailUserService.execute({ user_id });
 
     return res.json(user);
   }
--- src/services/user/DetailUserService.ts
+++ src/services/user/DetailUserService.ts
@@ -1,8 +1,11 @@
 import prismaClient from "../../prisma";
 
+interface UserRequest {
+  user_id: string;
+}
 class DetailUserService {
-  async execute(user_id: string) {
-    const user = await prismaClient.user.findFirst({
+  async execute({ user_id }: UserRequest) {
+    const user = await prismaClient.user.findUnique({
       where: {
         id: user_id,
       },
